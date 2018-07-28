c6e18e7d8e1d7de2a42f40dd11f5b3c0543ba65d
Saúl Cabrera <saulecabrera@gmail.com>
2018-07-28 15:18:51 -0400

    Misc
    Add banner
    Fix crash

--- .zshrc
+++ .zshrc
@@ -5,6 +5,7 @@ export ZSH=$HOME/.oh-my-zsh
 plugins=(
   git
 )
+
 ZSH_THEME="robbyrussell"
 source $ZSH/oh-my-zsh.sh
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,24 @@
 
 set -e
 
+cat << "EOF"
+
+ |  _ \ / _ \_   _|  ___|_ _| |   | ____/ ___| 
+ | | | | | | || | | |_   | || |   |  _| \___ \ 
+ | |_| | |_| || | |  _|  | || |___| |___ ___) |
+ |____/ \___/ |_| |_|   |___|_____|_____|____/ 
+  
+
+EOF
+
+printf "\n===> Pulling latest master...\n";
+
 git pull origin master;
+
+
+printf "\n===> Syncing new files to your home directory...\n";
 
 rsync --exclude ".git/" --exclude "README.md" --exclude ".gitignore" --exclude "bootstrap.sh" --exclude ".DS_Store" \
   -avh --no-perms . ~;
 
-# - Investigate why this crashes
-zshr;
+zsh;
